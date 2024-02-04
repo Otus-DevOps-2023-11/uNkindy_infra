4154b54a9f060d14da7554e31f558b1c9d194eb5
Станислав Кривошеев <skrivosheev@astralinux.ru>
2024-02-04 21:08:22 +0300

    HW 10 Main Commit

--- terraform/stage/inventory.tf
+++ terraform/stage/inventory.tf
@@ -1,9 +1,9 @@
 resource "local_file" "inventory" {
-  content = templatefile("templates/inventory.json.tpl",
+  content = templatefile("templates/inventory.yml.tpl",
     {
       app =  module.app.external_ip_address_app
       db  =  module.db.external_ip_address_db
     }
   )
-  filename = "../../ansible/inventory.json"
+  filename = "../../ansible/inventory.yml"
 }
